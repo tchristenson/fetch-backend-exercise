54183aa9e0ea23a080c9d13402d7b28b40ed66d2
Tommy Christenson <tchristenson21@gmail.com>
2023-08-16 14:17:40 -0500

    Basic server setup complete

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).send('Home Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('Resource not found')
+})
+
 
 
 
