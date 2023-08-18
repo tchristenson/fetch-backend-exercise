d47146a7d5481235f61552c1e0c8758c2c381c85
Tommy Christenson <tchristenson21@gmail.com>
2023-08-18 11:57:45 -0500

    Initializing receipt and item types

--- src/github.com/tchristenson/app/main.go
+++ src/github.com/tchristenson/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+type receipt struct{
+	ID           string 	`json: "id"`
+	Retailer 	 string 	`json: "retailer"`
+	PurchaseDate string 	`json: "purchaseDate"`
+	PurchaseTime string 	`json: "purchaseTime"`
+	Items 		 [...]map   `json: "items"`
+	Total 		 string 	`json: "total"`
+}
+
+type item struct{
+	ID           	 string `json: "id"`
+	ShortDescription string `json: "shortDescription"`
+	Price 			 string `json: "Price"`
+}
 
 func main() {
-	fmt.Println("Hello Go!!!!!")
+	router := gin.Default()
 }
