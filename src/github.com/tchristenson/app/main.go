1ad73df96dc360f73223b2e7525efcabc39aa7e7
Tommy Christenson <tchristenson21@gmail.com>
2023-08-18 17:54:49 -0500

    Initial Gin server successfuly running

--- src/github.com/tchristenson/app/main.go
+++ src/github.com/tchristenson/app/main.go
@@ -3,25 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"errors"
+	// "errors"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-type receipt struct{
-	ID           string 	`json: "id"`
-	Retailer 	 string 	`json: "retailer"`
-	PurchaseDate string 	`json: "purchaseDate"`
-	PurchaseTime string 	`json: "purchaseTime"`
-	Items 		 [...]map   `json: "items"`
-	Total 		 string 	`json: "total"`
+type receipt struct {
+	ID           string `json:"id"`
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []struct {
+		ShortDescription string `json:"shortDescription"`
+		Price            string `json:"price"`
+	} `json:"items`
+	Total string `json:"total"`
 }
 
-type item struct{
-	ID           	 string `json: "id"`
-	ShortDescription string `json: "shortDescription"`
-	Price 			 string `json: "Price"`
+type item struct {
+	ID               string `json:"id"`
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"Price"`
+}
+
+var receipts []receipt
+
+func processReceipt(c *gin.Context) {
+	uuid := uuid.New()
+	var newReceipt receipt
+	fmt.Println(uuid)
+
+	if err := c.BindJSON(&newReceipt); err != nil {
+		return
+	}
+
+	receipts = append(receipts, newReceipt)
+
+	c.IndentedJSON(http.StatusCreated, newReceipt)
 }
 
 func main() {
 	router := gin.Default()
+	router.POST("/receipts/process", processReceipt)
+	router.Run("localhost:5000")
 }
